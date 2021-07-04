2ad11abd050c147e8e7a44324cf003195fcb34fc
Frezcirno <2277861660@qq.com>
2021-07-04 22:14:10 +0800

    7.2

---
+++ tools/new.js
@@ -0,0 +1,44 @@
+"use strict";
+var exec = require('child_process').exec;
+
+function newLeetcode(id) {
+    let cmd = ['hexo', 'new', 'post', '-p', `leetcode/${id}`, `"${id}. title"`].join(' ');
+    exec(cmd, (error, stdout, stderr) => {
+        if (stdout) console.log(stdout);
+        if (stderr) console.log(stderr);
+        if (error) console.log(error);
+    });
+}
+
+function newDaily() {
+    let today = new Date(Date.now())
+    let date = today.toISOString().slice(0, 10)
+    let cmd = ['hexo', 'new', 'post', '-p', `daily/${date}`, "title"].join(' ');
+    exec(cmd, (error, stdout, stderr) => {
+        if (stdout) console.log(stdout);
+        if (stderr) console.log(stderr);
+        if (error) console.log(error);
+    });
+}
+
+function main() {
+    let args = process.argv.slice(2);
+    let type = args[0];
+
+    switch (type) {
+        case 'leetcode':
+        case 'lc':
+            let id = args[1];
+            newLeetcode(id);
+            break;
+        case 'daily':
+        case 'da':
+        case 'd':
+            newDaily();
+        default:
+            break;
+    }
+
+}
+
+main()
